bc1de2c8b1a3e888314580bcb20142a3f7fe259e
plamen <pgnikolov@gmail.com>
2024-06-23 04:09:10 +0200

    With OOP

---
+++ lists.py
@@ -0,0 +1,150 @@
+class Lists:
+
+    def __init__(self, elements: str):
+        self.elements = elements.split(", ")
+
+    def handle_append(self, new_element):
+        """
+            Appends a new element to the end of the list.
+        Args:
+            new_element (str): The element to be appended.
+        Returns:
+            The updated list.
+        """
+        self.elements.append(new_element)
+        return self.elements
+
+    def handle_extend(self, new_elements):
+        """
+            Extend a list by appending elements.
+        Args:
+            new_elements (str): The elements to be extended.
+        Returns:
+            The updated list.
+        """
+        self.elements.extend(new_elements)
+        return self.elements
+
+    def handle_insert(self, index: int, new_element):
+        """
+            Insert a new element into the list at a specified index.
+        Args:
+            index (int): The index to be inserted.
+            new_element (str): The element to be inserted.
+        Raises:
+            ValueError: If the input index is not an integer.
+        Returns:
+            updated list
+        """
+        try:
+            self.elements.insert(index, new_element)
+            return self.elements
+        except ValueError:
+            print("Invalid index. Please enter an integer.")
+
+    def handle_remove(self, element):
+        """
+            Remove an element from the list.
+        Args:
+            element (str): The element to be removed.
+        Raises:
+            ValueError: If the value is not found in the list.
+        Returns:
+            updated list
+        """
+        try:
+            self.elements.remove(element)
+            return self.elements
+        except ValueError:
+            print("Value not found in the list.")
+
+    def handle_pop(self, index=None):
+        """
+            Pop an element at a specified index or the last element if no index is provided.
+        Args:
+            index(int): The index to be popped.(optional)
+        Raises:
+            IndexError: If the index is out of range.
+            ValueError: If the input index is not an integer.
+        Returns:
+            updated list
+        """
+        try:
+            if not index:
+                self.elements.pop()
+            else:
+                self.elements.pop(index)
+            return self.elements
+        except (ValueError, IndexError):
+            print("Invalid index. Please enter an integer.")
+
+    def handle_clear(self):
+        """
+            Clear all elements from the list.
+        Argss:
+            list to clear.
+        Returns:
+            Print msg
+        """
+        self.elements.clear()
+        if not self.elements:
+            print("Empty list.")
+
+    def handle_index(self, element):
+        """
+            Find the index of a specified value in the list.
+        Args:
+            element (str): The element which index we want to be found.
+        Raises:
+            ValueError: If the value is not found in the list.
+        Returns:
+            index of value
+        """
+        try:
+            index = self.elements.index(element)
+            return f"The index of {element} is {index}."
+        except ValueError:
+            print("Value not found in the list.")
+
+    def handle_count(self, element):
+        """
+            Count the occurrences of a specified value in the list.
+        Args:
+            element (str): The element which index we want to count.
+        Returns:
+            number of ooccurrencess of a value
+        """
+        count = self.elements.count(element)
+        return f"The count of {element} is {count}."
+
+    def handle_sort(self):
+        """
+            Sort the list.
+        Raises:
+            TypeError: If the list contains elements of different data types.
+        Returns:
+            sorted list
+        """
+        try:
+            self.elements.sort()
+            return "Sorted list:", self.elements
+        except TypeError:
+            print("Cannot sort a list with different data types.")
+
+    def handle_reverse(self):
+        """
+            Reverse the order of elements in the list.
+        Returns:
+            reversed list
+        """
+        self.elements.reverse()
+        return f'Reversed list:", {self.elements}'
+
+    def handle_copy(self):
+        """
+            Create a copy of the list.
+        Returns:
+            list copy
+        """
+        copied_list = self.elements.copy()
+        return copied_list
--- main.py
+++ main.py
@@ -1,223 +1,95 @@
-def display_menu():
-	print("\nChoose a list operation:")
-	print("1. Append")
-	print("2. Extend")
-	print("3. Insert")
-	print("4. Remove")
-	print("5. Pop")
-	print("6. Clear")
-	print("7. Index")
-	print("8. Count")
-	print("9. Sort")
-	print("10. Reverse")
-	print("11. Copy")
-	print("12. Exit")
+from lists import Lists
 
 
-def handle_append(lst):
-	"""Appends a new element to the end of the list.
-	Args:
-		list to modify.
-	Returns:
-		The updated list.
-	"""
-	value = input("Enter a value to append: ")
-	lst.append(value)
-	print("Updated list:", lst)
+def lists_display_menu():
+    print("\nChoose a list operation:")
+    print("1. Append")
+    print("2. Extend")
+    print("3. Insert")
+    print("4. Remove")
+    print("5. Pop")
+    print("6. Clear")
+    print("7. Index")
+    print("8. Count")
+    print("9. Sort")
+    print("10. Reverse")
+    print("11. Copy")
+    print("12. Back to main menu")
 
 
-def handle_extend(lst):
-	"""Extend a list by appending elements from iterable.
-	Args:
-		list to modify.
-	Returns:
-		The updated list.
-	"""
-	values = input("Enter values to extend the list (comma-separated): ")
-	lst.extend(values.split(','))
-	print("Updated list:", lst)
+def list_menu():
+    initial_values = input("Enter initial list values (comma-separated): ")
+    elements = Lists(initial_values)
+
+    while True:
+        lists_display_menu()
+        choice = input("Enter your choice (1-12): ")
+        if choice == '1':
+            new_element = input("Enter element to be appended: ")
+            print(Lists.handle_append(elements, new_element))
+        elif choice == '2':
+            new_list = input("Enter comma-separated elements to be appended: ").split(", ")
+            print(Lists.handle_extend(elements, new_list))
+        elif choice == '3':
+            index_ = int(input("Enter index to be inserted: "))
+            new_element = input("Enter element to be inserted: ")
+            print(Lists.handle_insert(elements, index_, new_element))
+        elif choice == '4':
+            element = input("Enter element to be removed: ")
+            print(Lists.handle_remove(elements, element))
+        elif choice == '5':
+            index_ = int(input("Enter index to be removed or leave empty to remove last: "))
+            print(Lists.handle_pop(elements, index_))
+        elif choice == '6':
+            Lists.handle_clear(elements)
+        elif choice == '7':
+            element = input("Enter element to find its index: ")
+            print(Lists.handle_index(elements, element))
+        elif choice == '8':
+            element = input("Enter element to count: ")
+            print(Lists.handle_count(elements, element))
+        elif choice == '9':
+            print(Lists.handle_sort(elements))
+        elif choice == '10':
+            print(Lists.handle_reverse(elements))
+        elif choice == '11':
+            print(Lists.handle_copy(elements))
+        elif choice == '12':
+            print("Going back...")
+            main()
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
 
-def handle_insert(lst):
-	"""
-	Insert a new element into the list at a specified index.
-	Args:
-		list to insert the element into.
-	Raises:
-		ValueError: If the input index is not an integer.
-	Returns:
-		updated list
-	"""
-	try:
-		index = int(input("Enter the index to insert at: "))
-		value = input("Enter the value to insert: ")
-		lst.insert(index, value)
-		print("Updated list:", lst)
-	except ValueError:
-		print("Invalid index. Please enter an integer.")
-
-
-def handle_remove(lst):
-	"""
-	Remove an element from the list.
-	Args:
-		list to remove the element from.
-	Raises:
-		ValueError: If the value is not found in the list.
-	Returns:
-		updated list
-	"""
-	value = input("Enter a value to remove: ")
-	try:
-		lst.remove(value)
-		print("Updated list:", lst)
-	except ValueError:
-		print("Value not found in the list.")
-
-
-def handle_pop(lst):
-	"""
-	Pop an element at a specified index or the last element if no index is provided.
-	Args:
-		list to pop the element from.
-	Raises:
-		IndexError: If the index is out of range.
-		ValueError: If the input index is not an integer.
-	Returns:
-		updated list
-	"""
-	index = input("Enter the index to pop (leave empty to pop the last item): ")
-	try:
-		if index == "":
-			print("Popped value:", lst.pop())
-		else:
-			print("Popped value:", lst.pop(int(index)))
-		print("Updated list:", lst)
-	except IndexError:
-		print("Index out of range.")
-	except ValueError:
-		print("Invalid index. Please enter an integer.")
-
-
-def handle_clear(lst):
-	"""
-	Clear all elements from the list.
-	Argss:
-		list to clear.
-	Returns:
-		empty list
-	"""
-	lst.clear()
-	print("Updated list:", lst)
-
-
-def handle_index(lst):
-	"""
-	Find the index of a specified value in the list.
-	Args:
-		list to search for the value.
-	Raises:
-		ValueError: If the value is not found in the list.
-	Returns:
-		index of value
-	"""
-	value = input("Enter a value to find its index: ")
-	try:
-		index = lst.index(value)
-		print(f"The index of {value} is {index}.")
-	except ValueError:
-		print("Value not found in the list.")
-
-
-def handle_count(lst):
-	"""
-	Count the occurrences of a specified value in the list.
-	Args:
-		list to count the occurrences of the value.
-	Returns:
-		number of ooccurrencess of a value
-	"""
-	value = input("Enter a value to count: ")
-	count = lst.count(value)
-	print(f"The count of {value} is {count}.")
-
-
-def handle_sort(lst):
-	"""
-	Sort the list.
-	Argss:
-		list to sort.
-	Raises:
-		TypeError: If the list contains elements of different data types.
-	Returns:
-		sorted list
-	"""
-	try:
-		lst.sort()
-		print("Sorted list:", lst)
-	except TypeError:
-		print("Cannot sort a list with different data types.")
-
-
-def handle_reverse(lst):
-	"""
-	Reverse the order of elements in the list.
-	Args:
-		list to reverse.
-	Returns:
-		reversed list
-	"""
-	lst.reverse()
-	print("Reversed list:", lst)
-
-
-def handle_copy(lst):
-	"""
-	Create a copy of the list.
-	Argss:
-		list to copy.
-	Returns:
-		list copy
-	"""
-	copied_list = lst.copy()
-	print("Copied list:", copied_list)
+def display_main_menu():
+    print("\nChoose Data Structure:")
+    print("1. List")
+    print("2. Dictionary")
+    print("3. Set")
+    print("4. Tuple")
+    print("5. Exit")
 
 
 def main():
-	initial_values = input("Enter initial list values (comma-separated): ")
-	lst = initial_values.split(',')
+    while True:
+        display_main_menu()
+        user_choice = input("Enter your choice (1-4):")
 
-	while True:
-		display_menu()
-		choice = input("Enter your choice (1-12): ")
-		if choice == '1':
-			handle_append(lst)
-		elif choice == '2':
-			handle_extend(lst)
-		elif choice == '3':
-			handle_insert(lst)
-		elif choice == '4':
-			handle_remove(lst)
-		elif choice == '5':
-			handle_pop(lst)
-		elif choice == '6':
-			handle_clear(lst)
-		elif choice == '7':
-			handle_index(lst)
-		elif choice == '8':
-			handle_count(lst)
-		elif choice == '9':
-			handle_sort(lst)
-		elif choice == '10':
-			handle_reverse(lst)
-		elif choice == '11':
-			handle_copy(lst)
-		elif choice == '12':
-			print("Exiting the application.")
-			break
-		else:
-			print("Invalid choice. Please try again.")
+        if user_choice == '1':
+            list_menu()
+        elif user_choice == '2':
+            pass
+        elif user_choice == '3':
+            pass
+        elif user_choice == '4':
+            pass
+        elif user_choice == '5':
+            print("Exiting...!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
 
 if __name__ == "__main__":
-	main()
+    main()
